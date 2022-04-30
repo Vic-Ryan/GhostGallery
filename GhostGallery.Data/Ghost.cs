382d25715515c08635ef2a6c83592c0ad4e0303a
Victor Ryan <vryan9615@gmail.com>
2022-04-30 19:06:23 -0400

    Added ghost to Data layer

---
+++ GhostGallery.Data/Ghost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostGallery.Data
+{
+    public class Ghost
+    {
+        [Key]
+        public int GhostId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name ="Haunt Location")]
+        public int? Location { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        [Display(Name="First Sighting")]
+        public DateTimeOffset FirstSighting { get; set; }
+        [Required]
+        public string Appearance { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int ThreatLevel { get; set; }
+        [Required]
+        public string Powers { get; set; }
+    }
+}
